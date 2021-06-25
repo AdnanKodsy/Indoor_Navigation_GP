5179eb1c1414f900cd2c38382b4513e54d55baac
AdnanKodsy <64866292+AdnanKodsy@users.noreply.github.com>
2021-06-25 17:58:43 +0200

    intial project

---
+++ PathMarkAR-master/Assets/Scripts/ARPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ARPoint 
+{
+    public Vector3 pos;
+    public Vector3 fwd;
+
+    public Vector3 matT;
+    public ARPoint(Vector3 _pos, Vector3 _fwd, Vector3 refPoint)
+    {
+        pos = _pos;
+        fwd = _fwd;
+        matT = refPoint - pos;
+        HandleData();
+        matT.Normalize();
+    }
+
+    private void HandleData() {
+        pos -= matT;
+    }
+    public Vector3 GetARPoint(float dis) => pos + (dis * matT);
+}
---
+++ PathMarkAR-master/Assets/Scripts/ARPointSavedData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class ARPointSavedData
+{
+    public Vector3 refPoint;
+    public ARPoint[] points;
+
+    public ARPointSavedData(){}
+
+    public void PrepareDate(Vector3[] vectors, Vector3 _refPoint) {
+        refPoint = _refPoint;
+        points = new ARPoint[1000];
+        for (int i = 0; i < vectors.Length; i++)
+        {
+            var point = new ARPoint(vectors[i], Vector3.zero, refPoint);
+            points[i] = new ARPoint(vectors[i], Vector3.zero, refPoint);
+        }
+    }
+
+    public Vector3[] GetDate(Vector3 newRefPoint) {
+        Vector3[] vectors = new Vector3[points.Length];
+        var dis = Vector3.Distance(refPoint, newRefPoint);
+        for (int i = 0; i < points.Length; i++)
+        {
+            vectors[i] = points[i].GetARPoint(dis);
+        }
+        return vectors;
+    }
+}
